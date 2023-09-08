bcd22163db780540e7eee62b2b1413fb12b00f01
krishnadev7 <krishnadevv12@gmail.com>
2023-09-08 22:46:14 +0530

    contact form

---
+++ contact/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1>Get in touch</h1>
+        <p>Please fill in the fields</p>
+        <form action="contact.php" method="post">
+            <input type="text" name="name" placeholder="fullname">
+            <input type="text" name="email" placeholder="E-mail">
+            <input type="text" name="subject" placeholder="subject">
+            <textarea name="message" placeholder="Enter a message"></textarea>
+            <button type="submit" name="submit">Send E-mail</button>
+        </form>
+    </div>
+</body>
+</html>
